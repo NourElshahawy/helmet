6d180bc3e7abbb1011d05c17fa2842cda57ace52
Ahmed Elbadry <elbadry.egypt010@gmail.com>
2026-08-06 18:11:28 +0300

    bind about and provide

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,13 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://helmet.site.jadara.work/api",
-<<<<<<< HEAD
-    headers: {
-            Accept: "application/json",
-            "Accept-Language": "ar",
-        
-    }
-=======
 
      headers: {
     Accept: "application/json",
@@ -16,7 +9,6 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
   timeout: 10000,
->>>>>>> 3c7225862ce69b6d611882700a80b7704724c0b0
 });
 
 export default api;
