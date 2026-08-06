f5306818d50308eb52b2fddd28c5ee90af98863b
Mona <monameshref48@gmail.com>
2026-08-06 16:08:41 +0300

    marge

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,14 +2,12 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://helmet.site.jadara.work/api",
-    headers: { "Content-Type": "application/json" },
 
-
-    // headers: {
-    //     Accept: "application/json",
-    //     "Accept-Language": "ar",
-    //     // token: localStorage.getItem("userToken")
-    // }
+    headers: {
+        Accept: "application/json",
+        "Accept-Language": "ar",
+        // token: localStorage.getItem("userToken")
+    }
 });
 
 export default api;
